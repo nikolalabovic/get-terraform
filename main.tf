e0969adc815ca3c1f530e56d1460e91d9c8127f1
NL185095 <nikolalabovicpn@gmail.com>
2024-10-11 18:19:59 +0200

    edit subnet

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "test_vpc" {
 resource "aws_subnet" "test_subnet" {
   vpc_id = aws_vpc.test_vpc.id
   cidr_block = "10.123.1.0/24"
-  availability_zone = "eu-central-1"
+  availability_zone = "eu-central-1a"
 }
 
 resource "aws_internet_gateway" "test_gateway" {
